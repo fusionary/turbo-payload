3462f503aa1a1f9b4da23bf0fdc3f368bc741538
JP Grusling <jp.grusling@fusionary.com>
2025-02-24 18:21:14 -0500

    cleanup svelte eslint

--- tooling/eslint/svelte.mjs
+++ tooling/eslint/svelte.mjs
@@ -11,11 +11,15 @@ import base from '@local/eslint-config'
 /** @typedef {import('eslint').Linter.Config} Config */
 
 /** @type {Config[]} */
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+const baseConfig = base
+
+/** @type {Config[]} */
 const _default = [
   {
     ignores: ['build/', '.svelte-kit/', 'dist/'],
   },
-  ...base,
+  ...baseConfig,
   ...svelte.configs['flat/recommended'],
   eslintConfigPrettier,
   ...svelte.configs['flat/prettier'],
